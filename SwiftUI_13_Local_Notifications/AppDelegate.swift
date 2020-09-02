e2cc8a65f9c73cfa945b5caaa82dc1295f160244
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-02 18:10:07 +0200

    [FEATURE][LOCAL NOTIFICATION] * Foreground Notification was implemented

--- SwiftUI_13_Local_Notifications/AppDelegate.swift
+++ SwiftUI_13_Local_Notifications/AppDelegate.swift
@@ -34,5 +34,9 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
             NotificationCenter.default.post(name: NSNotification.Name("OpenView"), object: nil)
         }
     }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
 }
 
